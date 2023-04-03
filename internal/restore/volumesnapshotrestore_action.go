5af2fca3a29fbc21a19e0375aac75e96a948aa54
Tiger Kaovilai <passawit.kaovilai@gmail.com>
2023-04-03 17:03:28 -0400

    Revert "VSR restoreItemAction v2 (#12)"
    This reverts commit c0626897b1f451ddb53514314b87afa7ff659be5.

--- internal/restore/volumesnapshotrestore_action.go
+++ internal/restore/volumesnapshotrestore_action.go
@@ -2,35 +2,26 @@ package restore
 
 import (
 	"context"
-	"strings"
-	"time"
 
 	datamoverv1alpha1 "github.com/konveyor/volume-snapshot-mover/api/v1alpha1"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/vmware-tanzu/velero-plugin-for-csi/internal/util"
-	v1 "github.com/vmware-tanzu/velero/pkg/apis/velero/v1"
 	"github.com/vmware-tanzu/velero/pkg/plugin/velero"
-	riav2 "github.com/vmware-tanzu/velero/pkg/plugin/velero/restoreitemaction/v2"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-// VolumeSnapshotBackupRestoreItemActionV2 is a restore item action plugin to retrieve
+// VolumeSnapshotRestoreRestoreItemAction is a restore item action plugin to retrieve
 // VolumeSnapshotBackup from backup and create VolumeSnapshotRestore
-type VolumeSnapshotBackupRestoreItemActionV2 struct {
+type VolumeSnapshotRestoreRestoreItemAction struct {
 	Log logrus.FieldLogger
 }
 
-func (p *VolumeSnapshotBackupRestoreItemActionV2) Name() string {
-	return "VolumeSnapshotBackupRestoreItemActionV2"
-}
-
-// AppliesTo returns information indicating that the VolumeSnapshotBackupRestoreItemAction should be invoked
-func (p *VolumeSnapshotBackupRestoreItemActionV2) AppliesTo() (velero.ResourceSelector, error) {
-	p.Log.Info("VolumeSnapshotBackupRestoreItemAction AppliesTo")
+// AppliesTo returns information indicating that the VolumeSnapshotRestoreRestoreItemAction should be invoked
+func (p *VolumeSnapshotRestoreRestoreItemAction) AppliesTo() (velero.ResourceSelector, error) {
+	p.Log.Info("VolumeSnapshotRestoreRestoreItemAction AppliesTo")
 
 	return velero.ResourceSelector{
 		IncludedResources: []string{"volumesnapshotbackups.datamover.oadp.openshift.io"},
@@ -38,9 +29,9 @@ func (p *VolumeSnapshotBackupRestoreItemActionV2) AppliesTo() (velero.ResourceSe
 }
 
 // Execute backs up a VolumeSnapshotBackup object with a completely filled status
-func (p *VolumeSnapshotBackupRestoreItemActionV2) Execute(input *velero.RestoreItemActionExecuteInput) (*velero.RestoreItemActionExecuteOutput, error) {
+func (p *VolumeSnapshotRestoreRestoreItemAction) Execute(input *velero.RestoreItemActionExecuteInput) (*velero.RestoreItemActionExecuteOutput, error) {
 
-	p.Log.Infof("Executing VolumeSnapshotBackupRestoreItemActionV2")
+	p.Log.Infof("Executing VolumeSnapshotRestoreRestoreItemAction")
 	p.Log.Infof("Executing on item: %v", input.Item)
 	vsb := datamoverv1alpha1.VolumeSnapshotBackup{}
 
@@ -48,16 +39,18 @@ func (p *VolumeSnapshotBackupRestoreItemActionV2) Execute(input *velero.RestoreI
 		return &velero.RestoreItemActionExecuteOutput{}, errors.Wrapf(err, "failed to convert VSB input.Item from unstructured")
 	}
 
-	operationID := ""
+	snapMoverClient, err := util.GetVolumeSnapshotMoverClient()
+	if err != nil {
+		return nil, err
+	}
 
-	// create VSR per VSB
+	// create VSR
 	vsr := datamoverv1alpha1.VolumeSnapshotRestore{
 		ObjectMeta: metav1.ObjectMeta{
 			GenerateName: "vsr-",
 			Namespace:    vsb.Namespace,
 			Labels: map[string]string{
 				util.RestoreNameLabel:           input.Restore.Name,
-				util.BackupNameLabel:            vsb.Labels[util.BackupNameLabel],
 				util.PersistentVolumeClaimLabel: vsb.Annotations[util.VolumeSnapshotMoverSourcePVCName],
 			},
 		},
@@ -78,103 +71,19 @@ func (p *VolumeSnapshotBackupRestoreItemActionV2) Execute(input *velero.RestoreI
 		},
 	}
 
-	vsrClient, err := util.GetVolumeSnapshotMoverClient()
-	if err != nil {
-		return nil, err
-	}
-
 	// if namespace mapping is specified
 	if val, ok := input.Restore.Spec.NamespaceMapping[vsr.GetNamespace()]; ok {
 		vsr.SetNamespace(val)
 	}
 
-	err = vsrClient.Create(context.Background(), &vsr)
+	err = snapMoverClient.Create(context.Background(), &vsr)
 	if err != nil {
 		return nil, errors.Wrapf(err, "error creating volumesnapshotrestore CR")
 	}
 	p.Log.Infof("[vsb-restore] vsr created: %s", vsr.Name)
 
-	// fetch the VSR so we get the name of the VSR as we use generate name for VSR CR creation
-	err = vsrClient.Get(context.Background(), client.ObjectKey{Namespace: vsr.Namespace, Name: vsr.Name}, &vsr)
-	if err != nil {
-		return nil, errors.Wrapf(err, "error fetching volumesnapshotrestore CR for suppyling operationID")
-	}
-
-	// operationID for our datamover usecase is VSR NamespacedName which will unique per operation
-	operationID = vsr.Namespace + "/" + vsr.Name
-
-	p.Log.Info("Returning from VolumeSnapshotBackupRestoreItemActionV2")
-
 	// don't restore VSB
 	return &velero.RestoreItemActionExecuteOutput{
-		SkipRestore: true, OperationID: operationID,
+		SkipRestore: true,
 	}, nil
-}
-
-func (p *VolumeSnapshotBackupRestoreItemActionV2) Progress(operationID string, restore *v1.Restore) (velero.OperationProgress, error) {
-	progress := velero.OperationProgress{}
-
-	// handle empty operationID case
-	if operationID == "" {
-		return progress, riav2.InvalidOperationIDError(operationID)
-	}
-
-	// fetch the VSR matching the operationID supplied, read its status and return progress of datamovement
-	vsrClient, err := util.GetVolumeSnapshotMoverClient()
-	vsr := datamoverv1alpha1.VolumeSnapshotRestore{}
-	if err != nil {
-		return progress, errors.Wrapf(err, "error getting volumesnapshotrestore client")
-	}
-
-	splitOperationID := strings.Split(operationID, "/")
-	if len(splitOperationID) != 2 {
-		return progress, riav2.InvalidOperationIDError(operationID)
-	}
-
-	VSRNamespace := splitOperationID[0]
-	VSRName := splitOperationID[1]
-
-	err = vsrClient.Get(context.Background(), client.ObjectKey{Namespace: VSRNamespace, Name: VSRName}, &vsr)
-	if err != nil {
-		return progress, errors.Wrapf(err, "error fetching volumesnapshotrestore CR for operationID: %s", operationID)
-	}
-
-	// update progress status via VSR phases
-	if vsr.Status.Phase != "" && vsr.Status.BatchingStatus != "" {
-
-		progressDescriptionPhase := string(vsr.Status.Phase)
-		progressDescriptionBatchingStatus := string(vsr.Status.BatchingStatus)
-		progress.Description = "Phase: " + progressDescriptionPhase + " BatchingStatus: " + progressDescriptionBatchingStatus
-
-		p.Log.Infof("current progress description is: %s", progress.Description)
-
-		if vsr.Status.Phase == datamoverv1alpha1.SnapMoverRestorePhaseCompleted {
-			progress.Completed = true
-		}
-
-		if vsr.Status.Phase == datamoverv1alpha1.SnapMoverRestorePhaseFailed {
-			progress.Err = "VolumeSnapshotRestore has a failed status"
-			progress.Completed = true
-		}
-	}
-
-	// update progress timestamps
-	if vsr.Status.StartTimestamp != nil {
-		progress.Started = vsr.Status.StartTimestamp.Time
-	}
-
-	// mark updated timestamp
-	progress.Updated = time.Now()
-
-	return progress, nil
-}
-
-// empty func to satisfy riav2 interface
-func (p *VolumeSnapshotBackupRestoreItemActionV2) Cancel(operationID string, restore *v1.Restore) error {
-	return nil
-}
-
-// empty func to satisfy riav2 interface
-func (p *VolumeSnapshotBackupRestoreItemActionV2) AreAdditionalItemsReady(additionalItems []velero.ResourceIdentifier, restore *v1.Restore) (bool, error) {
-	return true, nil
 }
--- main.go
+++ main.go
@@ -31,7 +31,8 @@ func main() {
 		RegisterBackupItemActionV2("velero.io/vsm-volumesnapshotcontent-backupper", newVolumeSnapContentBackupItemActionV2).
 		RegisterBackupItemAction("velero.io/vsm-volumesnapshotbackup-backupper", newVolumeSnapshotBackupBackupItemAction).
 		RegisterRestoreItemAction("velero.io/vsm-volumesnapshot-restorer", newVolumeSnapshotRestoreItemAction).
-		RegisterRestoreItemActionV2("velero.io/vsm-datamover-restorer", newVolumeSnapshotBackupRestoreItemActionV2).
+		RegisterRestoreItemAction("velero.io/vsm-volumesnapshotclass-restorer", newVolumeSnapshotClassRestoreItemAction).
+		RegisterRestoreItemAction("velero.io/vsm-datamover-restorer", newVolumeSnapshotRestoreRestoreItemAction).
 		RegisterDeleteItemAction("velero.io/csi-volumesnapshotbackup-delete", newVolumeSnapshotBackupDeleteItemAction).
 		Serve()
 }
@@ -48,8 +49,12 @@ func newVolumeSnapshotRestoreItemAction(logger logrus.FieldLogger) (interface{},
 	return &restore.VolumeSnapshotRestoreItemAction{Log: logger}, nil
 }
 
-func newVolumeSnapshotBackupRestoreItemActionV2(logger logrus.FieldLogger) (interface{}, error) {
-	return &restore.VolumeSnapshotBackupRestoreItemActionV2{Log: logger}, nil
+func newVolumeSnapshotClassRestoreItemAction(logger logrus.FieldLogger) (interface{}, error) {
+	return &restore.VolumeSnapshotClassRestoreItemAction{Log: logger}, nil
+}
+
+func newVolumeSnapshotRestoreRestoreItemAction(logger logrus.FieldLogger) (interface{}, error) {
+	return &restore.VolumeSnapshotRestoreRestoreItemAction{Log: logger}, nil
 }
 
 func newVolumeSnapshotBackupDeleteItemAction(logger logrus.FieldLogger) (interface{}, error) {
